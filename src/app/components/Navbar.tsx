6ee0b4062d30105e162708c09050e8bce1eafe29
Fariha Asif <163842739+Fariha-Asif@users.noreply.github.com>
2024-10-12 18:46:40 +0500

    some changes occur in navbar file

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link"
-import Image from "next/image"
 export default function Navbar (){
     return(
         <header style={{backgroundColor:"blue" , color:"white", display:"flex", flexWrap:"wrap"}}>
